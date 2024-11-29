f1fde605870fa6fdaa11fae1497569777d40c9d1
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-11-30 01:07:37 +0200

    Add Create/Update/Delete/GetByIdAsync/GetAllAsync/ListManagersAsync methods Employee

---
+++ RestaurantReservation/Repositories/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class EmployeeRepository : IRepository<Employee>
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public EmployeeRepository(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Employee>> ListManagersAsync()
+    {
+        return await _context.Employees
+                             .Where(e => e.Position == "Manager")
+                             .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Employee>> GetAllAsync()
+    {
+        return await _context.Employees.ToListAsync();
+    }
+
+    public async Task<Employee> GetByIdAsync(int id)
+    {
+        return await _context.Employees.FindAsync(id);
+    }
+
+    public async Task CreateAsync(Employee employee)
+    {
+        await _context.Employees.AddAsync(employee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Employee employee)
+    {
+        _context.Employees.Update(employee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+        if (employee != null)
+        {
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
