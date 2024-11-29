f3a676b009a39ce36cd23b9fa4dfb1f0975cf0da
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-11-30 01:05:10 +0200

    Add Create/Update/Delete/GetByIdAsync/GetReservationsByCustomerAsync methods for Reservation

---
+++ RestaurantReservation/Repositories/ReservationRepository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class ReservationRepository : IRepository<Reservation>
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public ReservationRepository(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Reservation>> GetReservationsByCustomerAsync(int customerId)
+    {
+        return await _context.Reservations
+                             .Where(r => r.CustomerId == customerId)
+                             .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Reservation>> GetAllAsync()
+    {
+        return await _context.Reservations.ToListAsync();
+    }
+
+    public async Task<Reservation> GetByIdAsync(int id)
+    {
+        return await _context.Reservations.FindAsync(id);
+    }
+
+    public async Task CreateAsync(Reservation reservation)
+    {
+        await _context.Reservations.AddAsync(reservation);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Reservation reservation)
+    {
+        _context.Reservations.Update(reservation);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var reservation = await _context.Reservations.FindAsync(id);
+        if (reservation != null)
+        {
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    async Task<IEnumerable<Reservation>> IRepository<Reservation>.GetAllAsync()
+    {
+        return await _context.Reservations.ToListAsync();
+    }
+
+    async Task<Reservation> IRepository<Reservation>.GetByIdAsync(int id)
+    {
+        return await _context.Reservations.FindAsync(id);
+    }
+
+    async Task IRepository<Reservation>.CreateAsync(Reservation reservation)
+    {
+        await _context.Reservations.AddAsync(reservation);
+        await _context.SaveChangesAsync();
+    }
+
+    async Task IRepository<Reservation>.UpdateAsync(Reservation reservation)
+    {
+        _context.Reservations.Update(reservation);
+        await _context.SaveChangesAsync();
+    }
+
+    async Task IRepository<Reservation>.DeleteAsync(int id)
+    {
+        var reservation = await _context.Reservations.FindAsync(id);
+        if (reservation != null)
+        {
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
