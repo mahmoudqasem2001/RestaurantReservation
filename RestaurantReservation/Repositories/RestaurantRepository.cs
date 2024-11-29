fd4b25267716e28423a9508caf2ca5451ada4bcf
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-11-30 01:10:03 +0200

    Add Create/Update/Delete/GetByIdAsync/GetAllAsync methods for Restaurant

---
+++ RestaurantReservation/Repositories/RestaurantRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class RestaurantRepository : IRepository<Restaurant>
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public RestaurantRepository(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Restaurant>> GetAllAsync()
+    {
+        return await _context.Restaurants.ToListAsync();
+    }
+
+    public async Task<Restaurant> GetByIdAsync(int id)
+    {
+        return await _context.Restaurants.FindAsync(id);
+    }
+
+    public async Task CreateAsync(Restaurant restaurant)
+    {
+        await _context.Restaurants.AddAsync(restaurant);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Restaurant restaurant)
+    {
+        _context.Restaurants.Update(restaurant);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var restaurant = await _context.Restaurants.FindAsync(id);
+        if (restaurant != null)
+        {
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
