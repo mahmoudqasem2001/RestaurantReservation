f7457e1705c2a1b5e5e82c6b09318600b3e757f6
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-11-30 01:09:10 +0200

    Add Create/Update/Delete/GetAllAsync/ListOrderedMenuItemsAsync methods for OrderItem

---
+++ RestaurantReservation/Repositories/OrderItemRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class OrderItemRepository : IRepository<OrderItem>
+{
+    private readonly RestaurantReservationDbContext _context;
+
+    public OrderItemRepository(RestaurantReservationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<MenuItem>> ListOrderedMenuItemsAsync(int reservationId)
+    {
+        return await _context.OrderItems
+                             .Where(oi => oi.Order.ReservationId == reservationId)
+                             .Select(oi => oi.MenuItem)
+                             .ToListAsync();
+    }
+
+    public async Task<IEnumerable<OrderItem>> GetAllAsync()
+    {
+        return await _context.OrderItems.ToListAsync();
+    }
+
+    public async Task<OrderItem> GetByIdAsync(int id)
+    {
+        return await _context.OrderItems.FindAsync(id);
+    }
+
+    public async Task CreateAsync(OrderItem orderItem)
+    {
+        await _context.OrderItems.AddAsync(orderItem);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(OrderItem orderItem)
+    {
+        _context.OrderItems.Update(orderItem);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var orderItem = await _context.OrderItems.FindAsync(id);
+        if (orderItem != null)
+        {
+            _context.OrderItems.Remove(orderItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
